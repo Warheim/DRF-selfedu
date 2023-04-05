e9d513a5def720d4bc1a91b56e917ebb7176c3ad
Steshin <warheim24@gmail.com>
2023-04-05 15:23:34 +0300

    api tested with ApiView

--- women/views.py
+++ women/views.py
@@ -1,5 +1,8 @@
+from django.forms import model_to_dict
 from django.shortcuts import redirect
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from women.models import Woman
 from women.serializers import WomanSerializer
 
@@ -8,6 +11,31 @@ def index(request):
     return redirect('women')
 
 
-class WomanView(ListAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
+# class WomanView(ListAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
+
+
+class WomanView(APIView):
+    def get(self, request):
+        women = Woman.objects.all().values()
+        return Response({'posts': list(women)})
+
+    def post(self, request):
+        post = Woman.objects.create(
+            name=request.data['name'],
+            content=request.data['content'],
+            categories_id=request.data['categories'])
+        return Response({'post': model_to_dict(post)})
+
+
+class WomanRetrieveUpdateView(APIView):
+    def patch(self, request, pk):
+        Woman.objects.filter(id=pk).update(
+            content=request.data['content']
+        )
+        return Response({'status': 'OK'})
+
+    def delete(self, request, pk):
+        Woman.objects.filter(id=pk).delete()
+        return Response({'status': 'OK'})
